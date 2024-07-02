2e00a14ddfa7f3115ba9bfb334383b516b78a2ab
Guillaume Weghsteen <neuracr@gmail.com>
2024-07-02 15:32:05 +0000

    Add a base skeleton for the plugin

---
+++ safety-web/src/trusted_types_checks.ts
@@ -0,0 +1,48 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { ESLintUtils, ParserServicesWithTypeInformation } from '@typescript-eslint/utils';
+
+const createRule = ESLintUtils.RuleCreator(
+    () => 'safety-web',
+);
+
+/**
+ * Rule to check Trusted Types compliance
+ */
+export const trustedTypesChecks = createRule({
+    name: 'trusted-types-checks',
+    meta: {
+        type: 'problem',
+        docs: {
+            description: 'Checks Trusted Types compliance',
+            recommended: 'strict',
+        },
+        messages: {
+            // TODO: add the list of rules
+            unknown_rule_triggered: 'trusted-types-checks reported a violation that could not be mapped to a known violation id: {{ tsecMessage }}',
+        },
+        schema: [],
+    },
+    defaultOptions: [],
+    create(context) {
+        return {
+            // Naming corresponds to ESTree node type
+            // tslint:disable-next-line:enforce-name-casing
+            Program(node) {
+                // TODO: implement me
+            },
+        };
+    }
+});
