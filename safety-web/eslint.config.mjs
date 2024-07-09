c2119c51626a932f8faed737ca7e51170e5bacaf
Guillaume Weghsteen <neuracr@gmail.com>
2024-07-09 15:43:18 +0000

    Set ESLint up for safety-web sources
    Change-Id: Ieff5925cfd6ecfbc314dd69ad3d077cab0320ae1

---
+++ safety-web/eslint.config.mjs
@@ -0,0 +1,34 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "tsconfig.json",  // Indicates to find the closest tsconfig.json for each source file (see https://typescript-eslint.io/packages/parser#project).
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    files: ["**/*.ts"],
+  },
+  {
+    rules: {
+      'no-undef': 'off',
+      'no-dupe-class-members': 'off',
+    },
+    files: ['**/*.ts'],
+  },
+  {
+    ignores: [
+      "**/*.js",
+      "**/*.mjs",
+      "test/test_fixtures/",
+      "lib/",
+      "node_modules/",
+      "src/common/",  // tsetse folder is linted internally.
+    ]
+  },
+);
