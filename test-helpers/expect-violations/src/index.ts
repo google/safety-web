a79b0375c73bdaf071f78fa3110821618c18f1e2
Guillaume Weghsteen <neuracr@gmail.com>
2024-07-12 14:14:48 +0000

    Add commands to update the violation golden files
    Also explicitely use the node_module path for running eslint8 as there's a bug in yarn that makes it run the wrong binary https://github.com/yarnpkg/yarn/issues/8590
    
    Change-Id: I0ffb28f7831b8c016d1f6f00b9af9909d78647ed

--- test-helpers/expect-violations/src/index.ts
+++ test-helpers/expect-violations/src/index.ts
@@ -18,22 +18,49 @@ import { expect } from 'chai';
 import fs from 'node:fs';
 import type { ViolationReport } from './violations.js';
 import * as path from 'path';
+import yargs from 'yargs';
 
 function main() {
-  const argv = process.argv;
-  // TODO process with Yargs as more flags are added.
-  if (argv.length !== 3) {
-    console.error('Usage: <eslint JSON output> | npx expect-violations <path/to/expectated_violations.json>');
-    return;
-  }
-  const expectatedViolationReportPath = argv[2];
+  const options = yargs(process.argv.slice(2))
+    .scriptName('expect-violations')
+    .command('update <expected_file_json>', 'Updates the expected violation file')
+    .command('test <expected_file_json>', 'Test the expected violation file')
+    .demandCommand()
+    .parseSync();
+
+  const command = options._[0] as 'update' | 'test';
+  const expectatedViolationReportPath = options.expected_file_json as string;
+
   const expectedViolationReport = getExpectedViolations(expectatedViolationReportPath) as ViolationReport;
   const actualESLintReport = JSON.parse(fs.readFileSync(0).toString()) as ViolationReport; // STDIN_FILENO = 0
   const canonicalizedReport = canonicalizeViolationReport(actualESLintReport);
+  switch (command) {
+    case 'test':
+      test(canonicalizedReport, expectedViolationReport);
+      break;
+    case 'update':
+      update(canonicalizedReport, expectedViolationReport, expectatedViolationReportPath);
+      break;
+  }
+}
 
-
+function test(canonicalizedReport: ViolationReport, expectedViolationReport: ViolationReport) {
   expect(canonicalizedReport).to.deep.equal(expectedViolationReport);
+}
 
+function update(canonicalizedReport: ViolationReport, expectedViolationReport: ViolationReport, filePath: string) {
+  let isEqual = true;
+  try {
+    expect(JSON.parse(JSON.stringify(canonicalizedReport, null, 2))).to.deep.equal(expectedViolationReport);
+  } catch {
+    isEqual = false;
+  }
+  if (isEqual) {
+    console.log('Expected violations already up to date.');
+  } else {
+    fs.writeFileSync(filePath, JSON.stringify(canonicalizedReport, null, 2));
+    console.log(`Updated the violation file: ${filePath}`);
+  }
 }
 
 function canonicalizeViolationReport(report: ViolationReport): ViolationReport {
