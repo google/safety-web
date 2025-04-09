e9aed78852e532a1167d5c6517748557c3826f9a
neuracr <neuracr@gmail.com>
2025-04-09 11:42:47 +0000

    Add a flag to filter the summary for a single package
    Change-Id: I9721306357ce9d7cd37b574968cb8f10495dc4f3

--- packages/runner/src/index.ts
+++ packages/runner/src/index.ts
@@ -16,6 +16,7 @@
 import yargs from 'yargs';
 import {run, SAFETY_WEB_TSCONFIG_FILENAME} from './runner.js';
 import {Logger} from './logging.js';
+import {PackageSummary} from '@safety-web/types';
 
 async function parseCli() {
   return yargs(process.argv.slice(2))
@@ -32,6 +33,11 @@ async function parseCli() {
       describe: `Try to use an existing tsconfig.json in the project instead of creating/using the custom ${SAFETY_WEB_TSCONFIG_FILENAME} one`,
       type: 'boolean',
     })
+    .option('packageName', {
+      demandOption: false,
+      describe: 'Only output a safety-web summary this package name',
+      type: 'string',
+    })
     .command('run', 'run safety-web and report the violations')
     .demandCommand()
     .parse();
@@ -41,9 +47,36 @@ async function main() {
   const args = await parseCli();
   Logger.info(`Processing repository at ${args.rootDir}`);
   const summaries = await run(args.rootDir, args.useDefaultTSConfig);
-  Logger.info(
-    `Done processing. Summaries: ${JSON.stringify([...summaries], null, 2)}`,
-  );
+  if (args.packageName !== undefined) {
+    const summary = filterSummary(summaries, args.packageName);
+    Logger.info(
+      `Done processing. Summary for ${args.packageName}: ${JSON.stringify(summary, null, 2)}`,
+    );
+  } else {
+    Logger.info(
+      `Done processing. Summaries: ${JSON.stringify([...summaries], null, 2)}`,
+    );
+  }
+}
+
+function filterSummary(
+  summaries: Set<PackageSummary>,
+  packageName: string,
+): PackageSummary | undefined {
+  const pack = [...summaries].filter((s) => s.packageName === packageName);
+  if (pack.length === 0) {
+    Logger.error(
+      `Error while filtering summary for package ${packageName}. Package could not be found. Found packages: ${String([...summaries].map((s) => s.packageName))}`,
+    );
+    return undefined;
+  }
+  if (pack.length > 1) {
+    Logger.error(
+      `Error while filtering summary for package ${packageName}. Several packages with the same name found.`,
+    );
+    return undefined;
+  }
+  return pack[0];
 }
 
 main().catch((error) => {
