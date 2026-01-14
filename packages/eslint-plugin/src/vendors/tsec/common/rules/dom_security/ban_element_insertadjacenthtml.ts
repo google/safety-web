e55898f9db2ffaf16881ff107741387d460aeae8
kkmuffme <11071985+kkmuffme@users.noreply.github.com>
2026-01-14 11:55:38 +0100

    Allow .insertAdjacentHTML() with TrustedHTML

--- packages/eslint-plugin/src/vendors/tsec/common/rules/dom_security/ban_element_insertadjacenthtml.ts
+++ packages/eslint-plugin/src/vendors/tsec/common/rules/dom_security/ban_element_insertadjacenthtml.ts
@@ -12,35 +12,98 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {
-  ConformancePatternRule,
-  ErrorCode,
-  PatternKind,
-} from '../../third_party/tsetse/rules/conformance_pattern_rule';
-import {overridePatternConfig} from '../../third_party/tsetse/util/pattern_config';
+import {Allowlist} from '../../third_party/tsetse/allowlist';
+import {Checker} from '../../third_party/tsetse/checker';
+import {ErrorCode} from '../../third_party/tsetse/error_code';
+import {AbstractRule} from '../../third_party/tsetse/rule';
+import {shouldExamineNode} from '../../third_party/tsetse/util/ast_tools';
+import {isExpressionOfAllowedTrustedType} from '../../third_party/tsetse/util/is_trusted_type';
+import {PropertyMatcher} from '../../third_party/tsetse/util/property_matcher';
 import {TRUSTED_HTML} from '../../third_party/tsetse/util/trusted_types_configuration';
+import * as ts from 'typescript';
 
 import {RuleConfiguration} from '../../rule_configuration';
+
+const BANNED_PROPERTY = 'Element.prototype.insertAdjacentHTML';
 
 let errMsg = 'Do not use Element#insertAdjacentHTML, as this can lead to XSS.';
 
 /**
- * A Rule that looks for use of Element#insertAdjacentHTML properties.
+ * Checks if insertAdjacentHTML is called with a TrustedHTML value.
+ * The second argument (index 1) must be TrustedHTML.
  */
-export class Rule extends ConformancePatternRule {
+function isCalledWithTrustedHTML(n: ts.Node, tc: ts.TypeChecker): boolean {
+  const par = n.parent;
+  // Check if this is a call expression
+  if (!ts.isCallExpression(par) || par.expression !== n) return false;
+  // insertAdjacentHTML needs at least 2 arguments
+  if (par.arguments.length < 2) return false;
+
+  // Check if the second argument (the HTML string) is TrustedHTML
+  return isExpressionOfAllowedTrustedType(tc, par.arguments[1], TRUSTED_HTML);
+}
+
+function checkNode(
+  tc: ts.TypeChecker,
+  n: ts.PropertyAccessExpression | ts.ElementAccessExpression,
+  matcher: PropertyMatcher,
+): ts.Node | undefined {
+  if (!shouldExamineNode(n)) return;
+  if (!matcher.typeMatches(tc.getTypeAtLocation(n.expression))) return;
+  if (isCalledWithTrustedHTML(n, tc)) return;
+  return n;
+}
+
+/**
+ * A Rule that looks for use of Element#insertAdjacentHTML method.
+ */
+export class Rule extends AbstractRule {
   static readonly RULE_NAME = 'ban-element-insertadjacenthtml';
+  readonly ruleName = Rule.RULE_NAME;
+  readonly code = ErrorCode.CONFORMANCE_PATTERN;
+
+  private readonly propMatcher: PropertyMatcher;
+  private readonly allowlist?: Allowlist;
 
   constructor(configuration: RuleConfiguration = {}) {
-    super(
-      overridePatternConfig({
-        errorCode: ErrorCode.CONFORMANCE_PATTERN,
-        errorMessage: errMsg,
-        kind: PatternKind.BANNED_PROPERTY,
-        values: ['Element.prototype.insertAdjacentHTML'],
-        name: Rule.RULE_NAME,
-        allowedTrustedType: TRUSTED_HTML,
-        ...configuration,
-      }),
+    super();
+    this.propMatcher = PropertyMatcher.fromSpec(BANNED_PROPERTY);
+    if (configuration?.allowlistEntries) {
+      this.allowlist = new Allowlist(configuration?.allowlistEntries);
+    }
+  }
+
+  register(checker: Checker) {
+    checker.onNamedPropertyAccess(
+      this.propMatcher.bannedProperty,
+      (c, n) => {
+        const node = checkNode(c.typeChecker, n, this.propMatcher);
+        if (node) {
+          checker.addFailureAtNode(
+            node,
+            errMsg,
+            Rule.RULE_NAME,
+            this.allowlist,
+          );
+        }
+      },
+      this.code,
+    );
+
+    checker.onStringLiteralElementAccess(
+      this.propMatcher.bannedProperty,
+      (c, n) => {
+        const node = checkNode(c.typeChecker, n, this.propMatcher);
+        if (node) {
+          checker.addFailureAtNode(
+            node,
+            errMsg,
+            Rule.RULE_NAME,
+            this.allowlist,
+          );
+        }
+      },
+      this.code,
     );
   }
 }
